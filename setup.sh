9388c1ed992c48feb2b8f6d66691dbd52da630a8
Matthew Archer <matt.james.archer@gmail.com>
2014-12-26 15:31:31 +0000

    link change

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-rm .vim .bashrc .vimrc .gitconfig .xmonad .Xresources .screenrc .inputrc .profile git-completion.bash
+rm .vim .bashrc .vimrc .gitconfig .xmonad .Xresources .screenrc .inputrc .profile .git-completion.bash
 # rm .vim
 
 ln -s .dot/bashrc .bashrc
@@ -15,7 +15,7 @@ ln -s .dot/Xresources .Xresources
 ln -s .dot/screenrc .screenrc
 ln -s .dot/inputrc .inputrc
 ln -s .dot/profile .profile
-ln -s .dot/git/git-completion.bash git-completion.bash
+ln -s .dot/git/git-completion.bash .git-completion.bash
 # cd .ssh 
 # ln -s ../.dot/sshconfig config
 # cd ..
